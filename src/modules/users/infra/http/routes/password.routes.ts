2e94e841a73f4dca956e7588fe60cb4b556646cf
luizpaulino <lp.luizpaulino@gmail.com>
2020-08-02 23:58:08 -0300

    Corrigindo funcionalidade de reset de senha

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -21,7 +21,6 @@ passwordRouter.post(
   '/reset',
   celebrate({
     [Segments.BODY]: {
-      email: Joi.string().email().required(),
       token: Joi.string().uuid().required(),
       password: Joi.string().required(),
       password_confirmation: Joi.string().required().valid(Joi.ref('password')),
